36ee1dec068e5d4fd3c82a03ae232788f892a333
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-04-18 15:48:32 +0900

    :wrench: replace version.catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.0").apply(false)
-    id("com.android.library").version("8.0.0").apply(false)
-    kotlin("android").version("1.8.10").apply(false)
-    kotlin("multiplatform").version("1.8.10").apply(false)
+
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.androidGradlePlugin) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.androidGradleLibraryPlugin) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.kotlinPlugin) apply false
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.multiplatform) apply false
 }
 
 tasks.register("clean", Delete::class) {
