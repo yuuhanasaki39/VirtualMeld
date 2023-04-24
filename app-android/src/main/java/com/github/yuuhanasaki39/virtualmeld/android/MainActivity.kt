628cff5586258dd8f05940e48bbbcb758a06f7be
yuuhanasaki39 <yuuhanasaki39@gmail.com>
2023-04-25 02:33:39 +0900

    :wrench:add nav-app

--- app-android/src/main/java/com/github/yuuhanasaki39/virtualmeld/android/MainActivity.kt
+++ app-android/src/main/java/com/github/yuuhanasaki39/virtualmeld/android/MainActivity.kt
@@ -1,74 +1,30 @@
 package com.github.yuuhanasaki39.virtualmeld.android
 
 import android.os.Bundle
-import android.view.ViewGroup
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
+import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.unity3d.player.UnityPlayer
 
 class MainActivity : ComponentActivity() {
-    var unityPlayer: UnityPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContent {
-//            MyApplicationTheme {
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    GreetingView(Greeting().greet())
-//                }
-//            }
-//        }
 
-        setContentView(R.layout.unity_main)
-
-        unityPlayer = UnityPlayer(this)
-        window.clearFlags(1024)
-
-        findViewById<ConstraintLayout>(R.id.unity).addView(
-            unityPlayer,
-            ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-        )
-
-        unityPlayer?.requestFocus()
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        unityPlayer?.windowFocusChanged(hasFocus)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        unityPlayer?.resume()
+        setContent {
+            MyApplicationTheme() {
+                NavApp()
+            }
+        }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Column {
-        UnityView()
-        Text(text = text)
-    }
-}
 
-@Composable
-private fun UnityView(){
-}
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+
     }
 }
